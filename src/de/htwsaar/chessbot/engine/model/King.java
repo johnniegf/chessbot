7c170fd8ba78d79ddc399f77976527e3721e4724
Johannes Haupt <johnniegf@fsfe.org>
2015-06-10 21:10:47 +0200

    Zugfindung für den Köning begonnen.
        [+] KingTest.java: Testet die Zugfindung des Königs, unvollständig
        [#] King.java: Zugfindung implementiert.

--- src/de/htwsaar/chessbot/engine/model/King.java
+++ src/de/htwsaar/chessbot/engine/model/King.java
@@ -26,7 +26,24 @@ public class King extends Piece {
     }
 
     public Collection<Position> getValidMoves(Board context) {
-        return new ArrayList<Position>();
+        Collection<Position> validPositions = new ArrayList<Position>(8);
+        Position p = getPosition();
+        for (int x = -1; x <= 1; x++) {
+            for (int y = -1; y <= 1; y++) {
+                if ( x == 0 && y == 0 )
+                    continue;
+
+                try {
+                    validPositions.add( p.translate(x,y) );
+                } catch (RuntimeException rte) {}
+            }
+        }
+        if ( !hasMoved() ) {
+            validPositions.add( p.translate(0,2) );
+            validPositions.add( p.translate(0,-2) );
+        }
+
+        return validPositions; 
     }
 
     public String getName() {
---
+++ test/de/htwsaar/chessbot/engine/model/KingTest.java
@@ -0,0 +1,67 @@
+package de.htwsaar.chessbot.engine.model;
+
+import java.util.*;
+
+// JUnit-Pakete
+import org.junit.*;
+import static org.junit.Assert.*;
+
+/**
+* Testklasse für ...
+*
+* @author
+*/
+public class KingTest { 
+
+    // Testvariablen
+    private Piece whiteKingAtE1;
+
+    // Kontrollwerte
+    private static List<Position> possibleMovesAtE1;
+    private static final Board EMPTY_BOARD = new Board();
+
+    static {
+        possibleMovesAtE1 = new ArrayList<Position>(7);
+        possibleMovesAtE1.add( new Position(3,1) );
+        possibleMovesAtE1.add( new Position(4,1) );
+        possibleMovesAtE1.add( new Position(4,2) );
+        possibleMovesAtE1.add( new Position(5,2) );
+        possibleMovesAtE1.add( new Position(6,2) );
+        possibleMovesAtE1.add( new Position(6,1) );
+        possibleMovesAtE1.add( new Position(7,1) );
+    }
+
+    /**
+    * Testaufbau.
+    *
+    * Allokiert und initialisiert Ressourcen und Testvariablen.
+    */
+    @Before public void prepareTest() {
+        whiteKingAtE1 = new King(new Position(5,1), true, false); 
+    }
+
+    /**
+    * Testabbau.
+    *
+    * Gibt Ressourcen und Testvariablen frei.
+    */
+    @After public void cleanUp() {
+
+    }
+
+/* FIXTURES FÜR DIESEN TESTFALL */
+
+    // ====================================================
+    // = Funktionstests
+    // ====================================================
+
+    @Test public void testWhiteKingAtStartingPosition() {
+        Collection<Position> possibleMoves = whiteKingAtE1.getValidMoves(EMPTY_BOARD);
+        assertTrue("",
+                   possibleMovesAtE1.containsAll(possibleMoves) &&
+                   possibleMoves.containsAll(possibleMovesAtE1) ); 
+    }
+
+}
+
+
