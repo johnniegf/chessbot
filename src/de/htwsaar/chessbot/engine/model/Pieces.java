6afadf158102dcb717e22f4983a551d54bb26684
dholzapfel <david.holzapfel@hotmail.de>
2015-06-15 11:00:56 +0200

    Figurenfabrik und Testklasse
    Weitestgehende Fertigstellung der FIgurenfabrik und Erstellung der
    entsprechenden Testklasse.

--- src/de/htwsaar/chessbot/engine/model/Pieces.java
+++ src/de/htwsaar/chessbot/engine/model/Pieces.java
@@ -1,9 +1,24 @@
 package src.de.htwsaar.chessbot.engine.model;
 
+import com.sun.javaws.exceptions.InvalidArgumentException;
+
 import java.util.ArrayList;
 
+/**
+ * Fabrik zur Bereitstellung von Schachfiguren-Objekten.
+ *
+ * @author David Holzapfel
+ * @version 0.2
+ */
 public class Pieces {
 
+    //Fehlermeldungen
+    private static final String ERR_INVALID_PIECETYPE =
+            "Fehler: Ungueltiger Figurentyp: %d";
+    private static final String ERR_INVALID_POSITION =
+            "Fehler: Position ist 'null'";
+
+    //Konstanten für jeden der 6 Figurentypen
     public static final int PAWN = 0;
     public static final int ROOK = 1;
     public static final int KNIGHT = 2;
@@ -11,16 +26,23 @@ public class Pieces {
     public static final int QUEEN = 4;
     public static final int KING = 5;
 
+    //Klasseninstanz
     private static Pieces INSTANCE;
 
+    //Array, das einen Prototypen jedes Figurentyps enthält
     private Piece[] prototypes;
+    //Liste mit allen bisher erstellten Figurenobjekten
     private ArrayList<Piece> existingPieces;
 
+    //Privater Konstruktor, da die Klasse als Singleton implementiert ist.
     private Pieces() {
         this.prototypes = new Piece[6];
         this.existingPieces = new ArrayList<Piece>();
     }
 
+    /**
+     * @return Die Klasseninstanz der Fabrik
+     */
     public static Pieces getInstance() {
         if(INSTANCE == null) {
             INSTANCE = new Pieces();
@@ -29,8 +51,25 @@ public class Pieces {
         return INSTANCE;
     }
 
-    public Piece getPiece(int pieceType, Position position, boolean isWhite, boolean hasMoved) {
-        //TODO: Fehleberhandlung für ungültige Parameter
+    /**
+     * Prüft, ob ein Exemplar einer Schachfigur existiert und erstellt bei
+     * Bedarf ein neues Objekt.
+     *
+     * @param pieceType Figurentyp
+     * @param position  Position auf dem Schachbrett
+     * @param isWhite   Farbe der Schachfigur
+     * @param hasMoved  Ob die Figur bereits bewegt wurde
+     * @return  Ein Figurenobjekt mit den angegebenen Parametern
+     */
+    public Piece getPiece(int pieceType, Position position, boolean isWhite, boolean hasMoved)
+        throws InvalidArgumentException {
+        if(pieceType < 0 || pieceType > 5) {
+            throw new InvalidArgumentException(String.format(ERR_INVALID_PIECETYPE, pieceType));
+        }
+        if(position == null) {
+            throw new InvalidArgumentException(ERR_INVALID_POSITION);
+        }
+
         Piece piece = getExistingPiece(pieceType, position, isWhite, hasMoved);
         if(piece == null) {
             piece = this.getPrototypeCopy(pieceType);
@@ -42,8 +81,17 @@ public class Pieces {
         return piece;
     }
 
-    private Piece getExistingPiece(int pieceType, Position position, boolean isWhite, boolean hasMoved) {
-        //TODO: Fehleberhandlung für ungültige Parameter
+    //Prüft, ob ein Figurenobjekt bereits existiert. Ist das der Fall wird es zurückgegeben, wenn nicht wird
+    //null zurückgegeben.
+    private Piece getExistingPiece(int pieceType, Position position, boolean isWhite, boolean hasMoved)
+        throws InvalidArgumentException {
+        if(pieceType < 0 || pieceType > 5) {
+            throw new InvalidArgumentException(String.format(ERR_INVALID_PIECETYPE, pieceType));
+        }
+        if(position == null) {
+            throw new InvalidArgumentException(ERR_INVALID_POSITION);
+        }
+
         Piece foundPiece = null;
         for(int i = 0; i < existingPieces.size() && foundPiece == null; i++) {
             Piece piece = existingPieces.get(i);
@@ -55,8 +103,12 @@ public class Pieces {
         return foundPiece;
     }
 
-    private Piece getPrototypeCopy(int pieceType) {
-        //TODO: Fehlerbehandlung für ungültigen PieceType
+    //Erzeugt eine Kopie des Prototypen von einem bestimmten Figurentyp
+    private Piece getPrototypeCopy(int pieceType) throws InvalidArgumentException {
+        if(pieceType < 0 || pieceType > 5) {
+            throw new InvalidArgumentException(String.format(ERR_INVALID_PIECETYPE, pieceType));
+        }
+
         if(this.prototypes[pieceType] == null) {
             Piece newPrototype;
             switch(pieceType) {
---
+++ test/de/htwsaar/chessbot/engine/model/PiecesTest.java
@@ -0,0 +1,14 @@
+package test.de.htwsaar.chessbot.engine.model;
+
+import junit.framework.TestCase;
+
+import java.lang.Exception;
+import java.lang.Override;
+
+public class PiecesTest extends TestCase {
+
+    @Before
+    protected void setUp() throws Exception {
+
+    }
+}
