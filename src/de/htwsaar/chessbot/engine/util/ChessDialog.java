11cbfb6bd589813f6db0c2463a27d8546c53c56a
Dominik Becker <dominik.becker16@yahoo.de>
2015-08-18 14:48:01 +0200

    Implementierung UCI ChessDialog

---
+++ src/de/htwsaar/chessbot/engine/util/ChessDialog.java
@@ -0,0 +1,96 @@
+package de.htwsaar.chessbot.engine.util;
+import java.util.ArrayList;
+import java.util.Collection;
+import java.util.List;
+
+import de.htw.saarland.stl.Stdin;
+import de.htwsaar.chessbot.engine.model.Board;
+import de.htwsaar.chessbot.engine.model.BoardBuilder;
+import de.htwsaar.chessbot.engine.model.Game;
+
+public class ChessDialog {
+	//Konstanten
+	private static final int MOVE = 1;
+	private static final String NEWGAME = "ucinewgame";
+	private static final String NEWMOVE = "position";
+	private static final String QUIT = "quit";
+	
+	
+	private static BoardBuilder builder = Board.BUILDER;
+	private Game game;
+
+	public ChessDialog() {
+		this.game = new Game();
+	}
+	
+	private void exeCmd(String cmd) {
+		String[] cmds = cmd.split(" ");
+		for(int i = 0; i < cmds.length; i++) {
+			switch(cmds[i]) {
+			case NEWGAME:
+				this.game = new Game();
+				break;
+			case NEWMOVE:
+				List<String> moves;
+				if(cmds[i+1].equals("moves")) {
+					moves = new ArrayList<String>();
+					for(int j = i+2; j < cmds.length; j++) {
+						moves.add(cmds[j]);
+					}
+					move(moves);
+				}
+				else
+				if(cmds[i+1].equals("startpos")) {
+					this.game = new Game();
+					moves = new ArrayList<String>();
+					for(int j = i+3; j < cmds.length; j++) {
+						moves.add(cmds[j]);
+					}
+					move(moves);
+				} else {
+					this.game = new Game(cmds[i+2]);
+					moves = new ArrayList<String>();
+					for(int j = i+4; j < cmds.length; j++) {
+						moves.add(cmds[j]);
+					}
+					move(moves);
+				}
+				return;
+			case QUIT:
+				System.out.println("Spiel wurde beendet.");
+			
+			default	: ;
+		}
+		
+		}
+	}
+	
+	private String choseCmd() {
+		System.out.println("Kommando wahlen: \n----------------\n");
+		System.out.println("fuehre Zug aus: " + NEWMOVE + ";\n"+
+						   "neues Spiel: "+ NEWGAME+";\n"+
+						   "beenden: " + QUIT +";\n"+
+						   "-> ");
+		return Stdin.readlnString();
+	}
+	
+	private void start(){
+		String cmd = "";
+		while(!cmd.equals(QUIT)) {
+			cmd = choseCmd();
+			exeCmd(cmd);
+		}
+	}
+	
+	private void move(List<String> moves) {
+		for(int i = 0; i < moves.size(); i++) {
+			game.doMove(moves.get(i));
+		}
+		System.out.println(game.getCurrentBoard()+"\n\n\n");
+	}
+	
+	public static void main(String[] args) {
+		ChessDialog cd = new ChessDialog();
+		cd.start();
+	}
+}
